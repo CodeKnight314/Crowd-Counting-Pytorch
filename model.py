b473af7112d4479d9789061754e6f9f9af9708b7
CodeKnight314 <rtang24@nmhschool.org>
2024-07-24 21:10:10 -0700

    Remove redundant code

--- model.py
+++ model.py
@@ -18,25 +18,22 @@ class CRSNet(nn.Module):
 
     def forward(self, x): 
         x = self.frontend(x)
-        print(x.shape )
         x = self.backend(x)
         x = self.output_layer(x)
         return x
 
-    def make_layers(self, in_channels = 512, batch_norm=False, dilation = True):
+    def make_layers(self, in_channels = 512, batch_norm=True, dilation = True):
         if dilation:
             d_rate = 2
         else:
             d_rate = 1
+
         layers = []
         for v in self.backend_feat:
-            if v == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=d_rate,dilation = d_rate)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
-                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=d_rate,dilation = d_rate)
-                if batch_norm:
-                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-                else:
-                    layers += [conv2d, nn.ReLU(inplace=True)]
-                in_channels = v
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
         return nn.Sequential(*layers)
