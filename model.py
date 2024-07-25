86e2fc561775c3b459a32cbdafc74588cbe674ed
CodeKnight314 <tang.ri@northeastern.edu>
2024-07-24 21:41:55 -0700

    Change model name

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn 
 from torchvision.models import vgg19
 
-class CRSNet(nn.Module): 
+class CSRNet(nn.Module): 
     def __init__(self):
         super().__init__()
         
