838959edef9a6bb520a78f7750d587d718d7af06
CodeKnight314 <tang.ri@northeastern.edu>
2024-07-27 11:08:44 -0700

    Add inception module

--- model.py
+++ model.py
@@ -1,43 +1,89 @@
 import torch 
 import torch.nn as nn 
-from torchvision.models import vgg19
+from torchvision import models
 
-class CSRNet(nn.Module): 
-    def __init__(self, config : str = "B"):
-        super().__init__()
+class InceptionModule(nn.Module):
+    def __init__(self, in_channels):
+        super(InceptionModule, self).__init__()
+        self.branch1x1 = nn.Conv2d(in_channels, 64, kernel_size=1)
+
+        self.branch3x3 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=1),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        )
+
+        self.branch5x5 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=1),
+            nn.Conv2d(64, 64, kernel_size=5, padding=2)
+        )
+
+        self.branch_pool = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, 64, kernel_size=1)
+        )
         
-        self.frontend = vgg19(weights = 'IMAGENET1K_V1').features[0:23]
-        for params in self.frontend.parameters(): 
-            params.requires_grad = False
+    def forward(self, x):
+        branch1x1 = self.branch1x1(x)
+        branch3x3 = self.branch3x3(x)
+        branch5x5 = self.branch5x5(x)
+        branch_pool = self.branch_pool(x)
+        outputs = [branch1x1, branch3x3, branch5x5, branch_pool]
+        return torch.cat(outputs, 1)
 
-        self.backend_feat  = [512, 512, 512, 256, 128, 64]
+class CSRNet(nn.Module):
+    def __init__(self, load_weights=False):
+        super(CSRNet, self).__init__()
+        self.seen = 0
+        self.frontend_feat = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512]
+        self.backend_feat = [512, 512, 512, 256]
+        self.frontend = make_layers(self.frontend_feat)
+        self.backend = make_layers(self.backend_feat, in_channels = 512, dilation = True)
         
-        if config == "A":
-            self.d_rate = [1, 1, 1, 1, 1, 1]
-        elif config == "B": 
-            self.d_rate = [2, 2, 2, 2, 2, 2]
-        elif config == "C": 
-            self.d_rate = [2, 2, 2, 4, 4, 4]
-        elif config == "D": 
-            self.d_rate = [4, 4, 4, 4, 4, 4]
-
-        self.backend = self.make_layers()
+        self.inception_module = InceptionModule(256)
         
-        self.output_layer = nn.Conv2d(64, 1, kernel_size=1)
-
-    def forward(self, x): 
+        self.output_layer = nn.Sequential(*[nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+                                            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+                                            nn.Conv2d(64, 1, kernel_size=1)])
+        
+        if not load_weights:
+            mod = models.vgg16(weights='DEFAULT')
+            self._initialize_weights()
+            mod_state_dict = mod.state_dict()
+            frontend_state_dict = self.frontend.state_dict()
+            for k, v in frontend_state_dict.items():
+                if k in mod_state_dict:
+                    v.data[:] = mod_state_dict[k].data[:]
+    
+    def forward(self,x):
         x = self.frontend(x)
         x = self.backend(x)
         x = self.output_layer(x)
         return x
-
-    def make_layers(self, in_channels = 512, batch_norm=True):
-        layers = []
-        for v, d_rate in zip(self.backend_feat, self.d_rate):
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, std=0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)  
+                
+def make_layers(cfg, in_channels = 3, batch_norm=False, dilation = False):
+    if dilation:
+        d_rate = 2
+    else:
+        d_rate = 1
+    layers = []
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=d_rate, dilation = d_rate)
             if batch_norm:
                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
-        return nn.Sequential(*layers)
+    return nn.Sequential(*layers)
